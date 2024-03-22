2cfffa90b57e29ce3ab60012a257db8caadeb48f
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-03-22 22:29:59 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 
-let config =  { host: "127.0.0.1",
+let config =  { host: "127.0.0.1:3306",
 user: "root",
-password: "Ned0930519556",
+password: "password",
 database: "backoffice"    
 };
 
